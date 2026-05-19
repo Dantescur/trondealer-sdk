09a0e71f16b2cae56cd6b9d7ccc89e552416dc38
Dantescur <daniel@thedaniweb.eu.org>
2026-05-18 21:25:38 -0400

    feat: add swap resource with all endpoints

---
+++ src/resources/swap.ts
@@ -0,0 +1,35 @@
+import type { TronDealerHttpClient } from "../http";
+import type {
+  SwapPairsResponse,
+  SwapQuoteRequest,
+  SwapQuoteResponse,
+  SwapCreateRequest,
+  SwapCreateResponse,
+  SwapGetResponse,
+  SwapUnlockRequest,
+  SwapUnlockResponse,
+} from "../types";
+
+export class SwapResource {
+  constructor(private readonly http: TronDealerHttpClient) {}
+
+  pairs() {
+    return this.http.get<SwapPairsResponse>("/api/v2/swap/pairs");
+  }
+
+  quote(data: SwapQuoteRequest) {
+    return this.http.post<SwapQuoteResponse>("/api/v2/swap/quote", data);
+  }
+
+  create(data: SwapCreateRequest) {
+    return this.http.post<SwapCreateResponse>("/api/v2/swap/create", data);
+  }
+
+  get(id: string) {
+    return this.http.get<SwapGetResponse>(`/api/v2/swap/${id}`);
+  }
+
+  unlock(id: string, data: SwapUnlockRequest) {
+    return this.http.post<SwapUnlockResponse>(`/api/v2/swap/${id}/unlock`, data);
+  }
+}
